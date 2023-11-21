a6b3f40eb910a5128daebd6d46867e46d1c42f7d
Sebastian <sebastian.ampuerom@gmail.com>
2023-11-22 00:43:56 +0100

    update kill cmd

--- kill.sh
+++ kill.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 kill -9 $(ps aux | grep '[p]ython3 /home/pi/gpt-4chat/server.py' | awk '{print $2}')
+kill -9 $(ps aux | grep '/home/pi/gpt-4chat/venv/bin/python3' | awk '{print $2}')
+kill -9 $(ps aux | grep '/home/pi/gpt-4chat/venv/bin/python3' | awk '{print $2}')
