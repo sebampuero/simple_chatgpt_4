e27d6bd016450e45e671d5317c8f1cac07fe7239
Sebastian <sebastian.ampuerom@gmail.com>
2023-11-28 23:00:29 +0100

    feat: delete unnecessary env var

--- start_prd.sh
+++ start_prd.sh
@@ -5,7 +5,6 @@ export ENV=PROD
 export SUBDIRECTORY=gpt4
 export DOMAIN=sebampuerom.de
 export OPENAI_KEY=$OPENAPI_APIKEY
-export DB=chatgpt
 export DDB_CHATS_TABLE=chats
 export DDB_USERS_TABLE=authorized_users
 python3 /home/pi/gpt-4chat/server.py
