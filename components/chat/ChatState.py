18a4aadc7a34932ff946571af37722c85a7749b3
Sebastian <sebastian.ampuerom@gmail.com>
2024-03-27 01:38:38 +0100

    add redis for chat state management

--- components/chat/ChatState.py
+++ components/chat/ChatState.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from .RedisState import RedisState
 
 logger = logging.getLogger(__name__)
 
@@ -11,9 +12,7 @@ class ChatState:
             raise Exception("Singleton.")
         else:
             ChatState.__instance = self
-            self.map_ws_to_messages = {}
-            self.map_ws_to_timestamp = {} # ws id is mapped to a timestamp because that is used as sort key in dynamodb. Very tight coupled to the source, yes
-            self.map_ws_to_model = {}
+            self.redis = RedisState()
 
     @staticmethod
     def get_instance():
@@ -22,36 +21,19 @@ class ChatState:
         return ChatState.__instance
 
     def set_language_model(self, model: str, ws_id: str):
-        logger.debug(f"Set model {model} for ws id: {ws_id}")
-        self.map_ws_to_model[ws_id] = model
+        self.redis.set_language_model(model, ws_id)
 
     def get_language_model(self, ws_id: str) -> str:
-        if ws_id in self.map_ws_to_model:
-            return self.map_ws_to_model[ws_id]
-        return ""
+        return self.redis.get_language_model(ws_id)
 
     def append_message(self, item: dict, ws_id: str):
-        self.map_ws_to_messages[ws_id].append(item)
+        self.redis.append_message(item, ws_id)
     
     def set_messages_with_ts(self, messages: list, ws_id: str, timestamp: int):
-        self.map_ws_to_messages[ws_id] = messages
-        self.map_ws_to_timestamp[ws_id] = timestamp
-        logger.debug("Set new messages" + json.dumps(self.map_ws_to_messages[ws_id], indent=4))
-        logger.debug("Set new ws_id to timestamp " + json.dumps(self.map_ws_to_timestamp[ws_id], indent=4))
+        self.redis.set_messages_with_ts(messages, ws_id, timestamp)
 
-    def get_messages_with_ts(self, ws_id: str) -> list:
-        if ws_id in self.map_ws_to_messages and ws_id in self.map_ws_to_timestamp:
-            logger.debug("Retrieved message" + json.dumps(self.map_ws_to_messages[ws_id], indent=4))
-            logger.debug("Retrieved ws_id to timestamp" + json.dumps(self.map_ws_to_timestamp[ws_id], indent=4))
-            return {
-                "messages": self.map_ws_to_messages[ws_id],
-                "timestamp": self.map_ws_to_timestamp[ws_id]
-            }
-        return {}
+    def get_messages_with_ts(self, ws_id: str) -> dict:
+        return self.redis.get_messages_with_ts(ws_id)
 
     def remove_ws(self, ws_id: str):
-        messages = self.map_ws_to_messages.pop(ws_id, None)
-        ts = self.map_ws_to_timestamp.pop(ws_id, None)
-        self.map_ws_to_model.pop(ws_id, None)
-        logger.debug(f"Removed info to socket id {ws_id}")
-        logger.debug("Removed messages:" + str(messages))
+        self.redis.remove_ws(ws_id)
---
+++ components/chat/RedisState.py
@@ -0,0 +1,76 @@
+import redis
+import typing
+import logging
+import json
+from operator import itemgetter 
+
+logger = logging.getLogger(__name__)
+
+
+HOST = "192.168.0.14"
+PORT = 6379
+
+class RedisState():
+
+    def __init__(self):
+        self.r = redis.Redis(host=HOST, port=PORT, db=0, charset="utf-8", decode_responses=True)
+
+    def set_language_model(self, model: str, ws_id: str):
+        try:
+            self.r.hset(f"ws:{ws_id}", mapping={
+                "model": model
+            })
+        except redis.exceptions.RedisError as e:
+            logger.error(f"Error setting language model: {e}")
+
+    def get_language_model(self, ws_id: str) -> str:
+        try:
+            return self.r.hget(f"ws:{ws_id}", "model")
+        except redis.exceptions.RedisError as e:
+            logger.error(f"Error getting language model: {e}")
+            return ""
+
+    def append_message(self, item: dict, ws_id: str):
+        try:
+            msg_list = json.loads(self.r.hget(f"ws:{ws_id}", "messages"))
+            msg_list.append(item)
+            self.r.hset(f"ws:{ws_id}", mapping={
+                "messages": json.dumps(msg_list)
+            })
+        except redis.exceptions.RedisError as e:
+            logger.error(f"Error appending message: {e}")
+        except json.JSONDecodeError as e:
+            logger.error(f"Error decoding JSON: {e}")
+
+    def set_messages_with_ts(self, messages: list, ws_id: str, timestamp: int):
+        try:
+            messages = json.dumps(messages)
+            self.r.hset(f"ws:{ws_id}", mapping={
+                "messages": messages,
+                "timestamp": str(timestamp)
+            })
+        except redis.exceptions.RedisError as e:
+            logger.error(f"Error setting messages with timestamp: {e}")
+        except json.JSONDecodeError as e:
+            logger.error(f"Error encoding JSON: {e}")
+
+    def get_messages_with_ts(self, ws_id: str) -> dict:
+        try:
+            messages = json.loads(self.r.hget(f"ws:{ws_id}", "messages"))
+            timestamp = int(self.r.hget(f"ws:{ws_id}", "timestamp"))
+            return {
+                "messages": messages,
+                "timestamp": timestamp
+            }
+        except redis.exceptions.RedisError as e:
+            logger.error(f"Error getting messages with timestamp: {e}")
+            return {}
+        except json.JSONDecodeError as e:
+            logger.error(f"Error decoding JSON: {e}")
+            return {}
+
+    def remove_ws(self, ws_id: str):
+        try:
+            self.r.delete(f"ws:{ws_id}")
+        except redis.exceptions.RedisError as e:
+            logger.error(f"Error removing websocket ID {ws_id}: {e}")
