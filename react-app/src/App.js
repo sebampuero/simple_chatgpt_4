c4e4abb576944bf316c25d2f4779a8a60903f1b5
Sebastian <sebastian.ampuerom@gmail.com>
2025-02-24 21:26:27 +0000

    authorization of user should use interceptor too

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,15 @@
+import { fetchWithToken } from './api/api';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import EmailForm from './components/EmailForm';
-import ChatPage from './components/ChatPage';;
+import ChatPage from './components/ChatPage';
 
 function App() {
   const [isEmailAuthorized, setIsEmailAuthorized] = useState(false);
   const [email, setEmail] = useState(null);
 
   useEffect(() => {
-    fetch(`${process.env.PUBLIC_URL}/api/authorized-email`, {
+    fetchWithToken(`${process.env.PUBLIC_URL}/api/authorized-email`, {
       method: 'GET',
       headers: {
         "Content-Type": "application/json",
