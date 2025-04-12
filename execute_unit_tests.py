84557a90d7d150ff02893730df5bf83108fa5913
Sebastian <sebastian.ampuerom@gmail.com>
2025-04-12 13:30:21 +0000

    remove tests for now

--- execute_unit_tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-tests_dir = "components/tests"
-loader = unittest.TestLoader()
-tests = loader.discover(tests_dir, pattern="*Test.py")
-
-runner = unittest.TextTestRunner()
-result = runner.run(tests)
