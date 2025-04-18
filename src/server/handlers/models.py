c395a6c76aaf1c9da4984f277eea1bfbe2c22bbd
Sebastian <sebastian.ampuerom@gmail.com>
2025-04-18 10:21:36 +0000

    new styling and async open file

--- src/server/handlers/models.py
+++ src/server/handlers/models.py
@@ -1,9 +1,11 @@
 from sanic import Request
 from sanic.response import json as sanicjson
+from anyio import open_file
 import json
 
 
 async def get_available_models(request: Request):
-    with open("models_data.json", "r") as file:
-        models_data = json.load(file)
+    async with await open_file("models_data.json") as file:
+        contents = await file.read()
+        models_data = json.loads(contents)
     return sanicjson(models_data)
